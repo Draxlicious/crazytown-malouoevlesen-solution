4bef93ded11fccd10b77f3e174c6087123bed98e
Draxlicious <109243@RTS-365.dk>
2019-01-30 11:31:54 +0100

    opgave 2 done

---
+++ Lukas.solution/js/java.js
@@ -0,0 +1,104 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+// opgave 1 
+//     let i = 10
+//     let tekst = document.querySelector('.phase1')
+//     console.log(tekst)
+
+//     standard()
+ 
+    
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+//     alert("Its the final count down!")
+    
+// }, 10000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+    
+// }, 9000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 8000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 7000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 6000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 5000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 4000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 3000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 2000);
+// setTimeout(() => {
+//     i--
+//     console.log(i)
+//     tekst.innerHTML = i
+    
+    
+// }, 1000);
+// console.log(i)
+
+// function standard() {
+//     tekst.innerHTML = i
+// }
+
+// ===================================================================================================
+
+
+// phase 2
+
+let phase2Button = document.querySelector('.phase2')
+let donald = document.querySelector('.donald')
+
+
+
+phase2Button.addEventListener('click', () => {
+
+    donald.style.display = "block";
+
+})
+
+})
